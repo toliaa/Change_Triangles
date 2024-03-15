6526aa8c136f5a14a58ab662f6be8258742e4752
toliadriapak <toliadriapak@gmail.com>
2024-03-15 21:59:09 +0200

    Add project files.

---
+++ Change_Triangles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Rectangle rect1 = new Rectangle(5, 3, 0, 0);
+        Rectangle rect2 = new Rectangle(4, 2, 2, 1);
+        
+
+        // Moving rectangles
+        rect1.Move(2, 3);
+        Console.WriteLine($"Rectangle 1 new position: ({rect1.XCoordinate}, {rect1.YCoordinate})");
+
+        // Resizing rectangles
+        rect2.Resize(6, 4);
+        Console.WriteLine($"Rectangle 2 new dimensions: ({rect2.Length}, {rect2.Width})");
+
+        // Finding minimum enclosing rectangle
+        Rectangle minEnclosingRect = Rectangle.GetMinimumEnclosingRectangle(rect1, rect2);
+        Console.WriteLine($"Minimum enclosing rectangle dimensions: ({minEnclosingRect.Length}, {minEnclosingRect.Width})");
+
+        // Finding intersection rectangle
+        Rectangle intersectionRect = Rectangle.GetIntersection(rect1, rect2);
+        Console.WriteLine($"Intersection rectangle dimensions: ({intersectionRect.Length}, {intersectionRect.Width})");
+    }
+}
---
+++ Change_Triangles/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Rectangle
+{
+    public double Length { get; private set; }
+    public double Width { get; private set; }
+    public double XCoordinate { get; private set; }
+    public double YCoordinate { get; private set; }
+
+    public Rectangle(double length, double width, double xCoordinate, double yCoordinate)
+    {
+        Length = length;
+        Width = width;
+        XCoordinate = xCoordinate;
+        YCoordinate = yCoordinate;
+    }
+
+    public void Move(double deltaX, double deltaY)
+    {
+        XCoordinate += deltaX;
+        YCoordinate += deltaY;
+    }
+
+    public void Resize(double newLength, double newWidth)
+    {
+        Length = newLength;
+        Width = newWidth;
+    }
+
+    public static Rectangle GetMinimumEnclosingRectangle(Rectangle rect1, Rectangle rect2)
+    {
+        double minX = Math.Min(rect1.XCoordinate, rect2.XCoordinate);
+        double minY = Math.Min(rect1.YCoordinate, rect2.YCoordinate);
+        double maxX = Math.Max(rect1.XCoordinate + rect1.Length, rect2.XCoordinate + rect2.Length);
+        double maxY = Math.Max(rect1.YCoordinate + rect1.Width, rect2.YCoordinate + rect2.Width);
+
+        double enclosingLength = maxX - minX;
+        double enclosingWidth = maxY - minY;
+
+        return new Rectangle(enclosingLength, enclosingWidth, minX, minY);
+    }
+
+    public static Rectangle GetIntersection(Rectangle rect1, Rectangle rect2)
+    {
+        double minX = Math.Max(rect1.XCoordinate, rect2.XCoordinate);
+        double minY = Math.Max(rect1.YCoordinate, rect2.YCoordinate);
+        double maxX = Math.Min(rect1.XCoordinate + rect1.Length, rect2.XCoordinate + rect2.Length);
+        double maxY = Math.Min(rect1.YCoordinate + rect1.Width, rect2.YCoordinate + rect2.Width);
+
+        double intersectionLength = Math.Max(0, maxX - minX);
+        double intersectionWidth = Math.Max(0, maxY - minY);
+
+        return new Rectangle(intersectionLength, intersectionWidth, minX, minY);
+    }
+}
